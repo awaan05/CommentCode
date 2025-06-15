ba293a98548d8751a081599e760d075653f98a37
awaan05 <awaannisar0786@gmail.com>
2025-06-15 20:21:28 +0530

    Initial Commit

---
+++ CommentCode/CommentCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCodeApp.swift
+//  CommentCode
+//
+//  Created by Mohammad Awaan Nisar on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CommentCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CommentCode
+//
+//  Created by Mohammad Awaan Nisar on 15/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
